3ccd9ed5b078a3ca5b1e4ee94698b27f69bb5f90
Jaime Garcia de Enterria <jaimenterria@gmail.com>
2024-05-16 14:02:48 +0200

    dependencies added

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -51,6 +51,18 @@ android {
 
 dependencies {
 
+    //Compose ViewModel
+    implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.6.2")
+
+    //Network calls
+    implementation("com.squareup.retrofit2:retrofit:2.9.0")
+
+    //JSON to Kotlin object mapping
+    implementation("com.squareup.retrofit2:converter-gson:2.9.0")
+
+    //Image loading
+    implementation("io.coil-kt:coil-compose:2.4.0")
+
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
